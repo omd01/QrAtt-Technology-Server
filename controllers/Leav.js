c36e378943892488a3a905ff3ae36f8ec362b4b9
Om Dahale <odtech.in@gmail.com>
2023-02-05 22:36:49 -0800

    bug in leave solved

--- controllers/Leav.js
+++ controllers/Leav.js
@@ -45,7 +45,7 @@ export const myLeav = async (req, res) => {
                   return res.status(200).json({ success: true, message:"Success",data:leav});
 
                 }
-                return res.status(400).json({ success: false, message: "You Do Not Have Any Requests" });
+                return res.status(200).json({ success: false});
 
             }
 
@@ -55,7 +55,7 @@ export const myLeav = async (req, res) => {
         if (leav[0]) {
            return res.status(200).json({ success: true, message:"Success",data:leav});
         }
-        return res.status(400).json({ success: false, message: "You Do Not Have Any Requests" });
+        return res.status(400).json({ success: false});
         
        
 
