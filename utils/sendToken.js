a94eded279a97e7d406b494f85b36c572af0ace5
omd01 <omdahale01@gmail.com>
2023-07-17 00:32:34 -0700

    removed jwt token 1 month validity

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -3,8 +3,7 @@ export const sendToken =(res,user,statusCode,message) =>{
     const token = user.getJWTToken();
 
     const options ={
-        httpOnly : true,
-        expires : new Date(Date.now()+ process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
+        httpOnly : true
     }
 
     const userData = {
